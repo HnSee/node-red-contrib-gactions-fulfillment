10edddcd5bc794eca328a5db21720eb345e57582
HnSee <34213265+HnSee@users.noreply.github.com>
2021-04-19 10:07:02 +0200

    Add http/https server switch functionality

--- nodes/google-actions-server.js
+++ nodes/google-actions-server.js
@@ -2,6 +2,7 @@ module.exports = function(RED) {
 
     const express                   = require('express');
     const bodyParser                = require('body-parser');
+    const http                      = require('http');
     const https                     = require('https');
     const fs                        = require('fs');
     const events                    = require('events');
@@ -12,6 +13,7 @@ module.exports = function(RED) {
 
         this.path       = n.path || '/';
         this.port       = n.port || 8090;
+        this.https      = n.https || false;
         this.privateKey = n.privateKey || null;
         this.cert       = n.cert || null;
 
@@ -21,40 +23,57 @@ module.exports = function(RED) {
 
         var app = express().use(bodyParser.json());
 
-        var serverOptions = {
-            key: fs.readFileSync(this.privateKey),
-            cert: fs.readFileSync(this.cert)
-        };
+        var serverOptions;
+        
+        if (this.https) {
+            serverOptions = {
+                key: fs.readFileSync(this.privateKey),
+                cert: fs.readFileSync(this.cert)
+            };
+        }
 
         app.post(this.path, (req, res) => {
 
-            var conversation = {
-                handler: req.body.handler.name,
-                intent: req.body.intent,
-                lang: req.body.session.languageCode || req.body.user.locale,
-                response: {
-                    session: {
-                        id: req.body.session.id,
-                        params: {}
-                    },
-                    prompt: {
-                        override: false
+            if (this.events.listenerCount(req.body.handler.name) > 0) {
+
+                var conversation = {
+                    handler: req.body.handler.name,
+                    intent: req.body.intent,
+                    lang: req.body.session.languageCode || req.body.user.locale,
+                    response: {
+                        session: {
+                            id: req.body.session.id,
+                            params: {}
+                        },
+                        prompt: {
+                            override: false
+                        }
                     }
-                }
-            };
+                };
 
-            this.sessions.set(req.body.session.id, {
-                request: req,
-                response: res
-            });
+                this.sessions.set(req.body.session.id, {
+                    request: req,
+                    response: res
+                });
 
-            this.events.emit(req.body.handler.name, req.body.session.id, conversation);
+                this.events.emit(req.body.handler.name, req.body.session.id, conversation);
+
+            } else {
+
+                res.status(404).send("No handler for '" + req.body.handler.name + "' found.");
+
+            }
 
         });
 
         try {
 
-            this.server = https.createServer(serverOptions, app);
+            if (this.https) {
+                this.server = https.createServer(serverOptions, app);
+            } else {
+                this.server = http.createServer(app);
+            }
+            
             this.server.listen(this.port);
 
         } catch (e) {
